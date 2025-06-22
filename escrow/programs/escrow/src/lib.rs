b81c396f3505dcdf1e87a4d04edb49a761782121
h-lakshman <lakshman.vaibhav007@gmail.com>
2025-06-22 17:37:59 +0530

    implemented toke ninterface of anchor_spl

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -1,7 +1,9 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
-
-declare_id!("CG4Ufmj9r1W1YjYRGjUhnx6xqo2AG1UWZte94TstxswS");
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked},
+};
+declare_id!("94bir7datCRBc78Q9R1A7hgpSbW6iJqHPj2rSNnfBFeQ");
 
 #[program]
 pub mod escrow_program {
@@ -12,17 +14,18 @@ pub mod escrow_program {
 
         let cpi_ctx = CpiContext::new(
             ctx.accounts.token_program.to_account_info(),
-            Transfer {
+            TransferChecked {
                 from: ctx.accounts.initializer_token_account.to_account_info(),
                 to: ctx.accounts.escrow_vault.to_account_info(),
                 authority: ctx.accounts.initializer.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
             },
         );
-        token::transfer(cpi_ctx, amount)?;
+        transfer_checked(cpi_ctx, amount, ctx.accounts.mint.decimals)?;
 
         escrow.initializer = ctx.accounts.initializer.key();
         escrow.vault = ctx.accounts.escrow_vault.key();
-        escrow.mint = ctx.accounts.initializer_token_account.mint;
+        escrow.mint = ctx.accounts.mint.key();
         escrow.amount = amount;
         escrow.bump = ctx.bumps.escrow_state;
         escrow.is_active = true;
@@ -40,16 +43,6 @@ pub mod escrow_program {
             EscrowError::VaultBalanceMismatch
         );
 
-        let cpi_ctx_1 = CpiContext::new(
-            ctx.accounts.token_program.to_account_info(),
-            Transfer {
-                from: ctx.accounts.taker_token_account.to_account_info(),
-                to: ctx.accounts.initializer_token_account.to_account_info(),
-                authority: ctx.accounts.taker.to_account_info(),
-            },
-        );
-        token::transfer(cpi_ctx_1, escrow.amount)?;
-
         let bump_seed = [escrow.bump];
         let signer_seeds: &[&[&[u8]]] = &[&[
             b"escrow",
@@ -57,16 +50,17 @@ pub mod escrow_program {
             escrow.mint.as_ref(),
             &bump_seed,
         ]];
-        let cpi_ctx_2 = CpiContext::new_with_signer(
+        let cpi_ctx = CpiContext::new_with_signer(
             ctx.accounts.token_program.to_account_info(),
-            Transfer {
+            TransferChecked {
                 from: ctx.accounts.escrow_vault.to_account_info(),
                 to: ctx.accounts.taker_token_account.to_account_info(),
                 authority: ctx.accounts.escrow_state.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
             },
             signer_seeds,
         );
-        token::transfer(cpi_ctx_2, escrow.amount)?;
+        transfer_checked(cpi_ctx, escrow.amount, ctx.accounts.mint.decimals)?;
 
         ctx.accounts.escrow_state.is_active = false;
 
@@ -87,14 +81,15 @@ pub mod escrow_program {
         ]];
         let cpi_ctx = CpiContext::new_with_signer(
             ctx.accounts.token_program.to_account_info(),
-            Transfer {
+            TransferChecked {
                 from: ctx.accounts.escrow_vault.to_account_info(),
                 to: ctx.accounts.initializer_token_account.to_account_info(),
                 authority: ctx.accounts.escrow_state.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
             },
             signer_seeds,
         );
-        token::transfer(cpi_ctx, escrow.amount)?;
+        transfer_checked(cpi_ctx, escrow.amount, ctx.accounts.mint.decimals)?;
 
         ctx.accounts.escrow_state.is_active = false;
 
@@ -107,29 +102,36 @@ pub struct InitializeEscrow<'info> {
     #[account(mut)]
     pub initializer: Signer<'info>,
 
-    #[account(
-        mut,
-        constraint = initializer_token_account.owner == initializer.key()
-    )]
-    pub initializer_token_account: Account<'info, TokenAccount>,
+    pub mint: InterfaceAccount<'info, Mint>,
 
     #[account(
         mut,
-        constraint = escrow_vault.owner == escrow_state.key(),
-        constraint = escrow_vault.mint == initializer_token_account.mint
+        associated_token::mint = mint,
+        associated_token::authority = initializer,
+        associated_token::token_program = token_program,
     )]
-    pub escrow_vault: Account<'info, TokenAccount>,
+    pub initializer_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init,
+        payer = initializer,
+        token::mint = mint,
+        token::authority = escrow_state,
+        token::token_program = token_program,
+    )]
+    pub escrow_vault: InterfaceAccount<'info, TokenAccount>,
 
     #[account(
         init,
         payer = initializer,
         space = 8 + std::mem::size_of::<EscrowState>(),
-        seeds = [b"escrow", initializer.key().as_ref(), initializer_token_account.mint.as_ref()],
+        seeds = [b"escrow", initializer.key().as_ref(), mint.key().as_ref()],
         bump
     )]
     pub escrow_state: Account<'info, EscrowState>,
 
-    pub token_program: Program<'info, Token>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
 }
@@ -138,12 +140,24 @@ pub struct InitializeEscrow<'info> {
 pub struct Exchange<'info> {
     #[account(mut)]
     pub taker: Signer<'info>,
+    /// CHECK: This is the initializer account
+    pub initializer: AccountInfo<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
 
-    #[account(mut, constraint = taker_token_account.owner == taker.key())]
-    pub taker_token_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program,
+    )]
+    pub taker_token_account: InterfaceAccount<'info, TokenAccount>,
 
-    #[account(mut)]
-    pub initializer_token_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        seeds = [b"escrow",initializer.key().as_ref(),mint.key().as_ref()],
+        bump = escrow_state.bump
+    )]
+    pub escrow_state: Account<'info, EscrowState>,
 
     #[account(
         mut,
@@ -151,16 +165,10 @@ pub struct Exchange<'info> {
         constraint = escrow_vault.mint == escrow_state.mint,
         constraint = escrow_vault.owner == escrow_state.key()
     )]
-    pub escrow_vault: Account<'info, TokenAccount>,
+    pub escrow_vault: InterfaceAccount<'info, TokenAccount>,
 
-    #[account(
-        mut,
-        seeds = [b"escrow", escrow_state.initializer.as_ref(), escrow_state.mint.as_ref()],
-        bump = escrow_state.bump
-    )]
-    pub escrow_state: Account<'info, EscrowState>,
-
-    pub token_program: Program<'info, Token>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 #[derive(Accounts)]
@@ -168,11 +176,15 @@ pub struct Cancel<'info> {
     #[account(mut)]
     pub initializer: Signer<'info>,
 
+    pub mint: InterfaceAccount<'info, Mint>,
+
     #[account(
         mut,
-        constraint = initializer_token_account.owner == initializer.key()
+        associated_token::mint = mint,
+        associated_token::authority = initializer,
+        associated_token::token_program = token_program,
     )]
-    pub initializer_token_account: Account<'info, TokenAccount>,
+    pub initializer_token_account: InterfaceAccount<'info, TokenAccount>,
 
     #[account(
         mut,
@@ -180,7 +192,7 @@ pub struct Cancel<'info> {
         constraint = escrow_vault.mint == escrow_state.mint,
         constraint = escrow_vault.owner == escrow_state.key()
     )]
-    pub escrow_vault: Account<'info, TokenAccount>,
+    pub escrow_vault: InterfaceAccount<'info, TokenAccount>,
 
     #[account(
         mut,
@@ -189,7 +201,8 @@ pub struct Cancel<'info> {
     )]
     pub escrow_state: Account<'info, EscrowState>,
 
-    pub token_program: Program<'info, Token>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 #[account]
